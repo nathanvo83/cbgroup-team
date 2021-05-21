ac33936942f80572f44bdc79d0a97bdeda0246a8
nathan.vo@augensoftwaregroup.com <nathan.vo@augensoftwaregroup.com>
2021-05-22 10:25:10 +1200

    update home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import OurProjects from "./components/OurProjects/OurProjects";
-import ProcessStep from "./components/ProcessStep/ProcessStep";
-import Request from "../../components/Request/Request";
-import Slogan from "./components/Slogan/Slogan";
+// import OurProjects from "./components/OurProjects/OurProjects";
+// import ProcessStep from "./components/ProcessStep/ProcessStep";
+// import Request from "../../components/Request/Request";
+// import Slogan from "./components/Slogan/Slogan";
 import "./Home.css";
 
 function Home() {
@@ -12,10 +12,10 @@ function Home() {
   return (
     <div className="home">
       {/* <div className="page-header">Home</div> */}
-      <Slogan></Slogan>
+      {/* <Slogan></Slogan>
       <Request text={requestText}></Request>
       <ProcessStep></ProcessStep>
-      <OurProjects></OurProjects>
+      <OurProjects></OurProjects> */}
       {/* <Request text={requestTexts[1]}></Request> */}
     </div>
   );
