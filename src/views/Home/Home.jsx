d9e3400b909557a22f9187a644d9aab11072777b
nathan.vo@augensoftwaregroup.com <nathan.vo@augensoftwaregroup.com>
2021-05-22 10:30:26 +1200

    1

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,24 +1,8 @@
 import React from "react";
-// import OurProjects from "./components/OurProjects/OurProjects";
-// import ProcessStep from "./components/ProcessStep/ProcessStep";
-// import Request from "../../components/Request/Request";
-// import Slogan from "./components/Slogan/Slogan";
 import "./Home.css";
 
 function Home() {
-  // const requestText =
-  //   "UNDERSTAND WHAT GOES INTO A BUILDING PROJECT BEFORE YOU START";
-
-  return (
-    <div className="home">
-      {/* <div className="page-header">Home</div> */}
-      {/* <Slogan></Slogan>
-      <Request text={requestText}></Request>
-      <ProcessStep></ProcessStep>
-      <OurProjects></OurProjects> */}
-      {/* <Request text={requestTexts[1]}></Request> */}
-    </div>
-  );
+  return <div className="home">123</div>;
 }
 
 export default Home;
